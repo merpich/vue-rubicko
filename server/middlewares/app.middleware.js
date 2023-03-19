95f0a0bdb3651e0f404506528ef794438ce34996
merpich <merpich.dev@gmail.com>
2023-03-19 14:20:26 +0300

    rename uploadFile -> uploadFile.middleware

--- server/middlewares/app.middleware.js
+++ server/middlewares/app.middleware.js
@@ -1,6 +1,6 @@
 import handleValidationErrors from './handleValidationErrors.middleware.js'
 import checkAuth from './checkAuth.middleware.js'
-import uploadFile from './uploadFile.js'
+import uploadFile from './uploadFile.middleware.js'
 
 export {
 	handleValidationErrors,
