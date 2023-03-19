591741327520f9e5439ce24224090fd3aff1523e
merpich <merpich.dev@gmail.com>
2023-03-19 15:27:00 +0300

    crate model for post, category;

---
+++ server/models/category.model.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+
+const categoryShema = new mongoose.Schema({
+	title: { type: String, required: true }
+})
+
+export default mongoose.Schema('Category', categoryShema)
---
+++ server/models/post.model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const postShema = new mongoose.Schema({
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+	title: { type: String, required: true },
+	imageUrl: { type: String, required: false },
+	liked: { type: Array, required: true, default: [] }
+}, {
+	timestamps: true
+})
+
+export default mongoose.model('Post', postShema)
