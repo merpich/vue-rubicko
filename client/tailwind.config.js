f9688b84b156fe3f5b26ca6ba1c6fdf622539748
merpich <merpich.dev@gmail.com>
2023-03-23 23:48:16 +0300

    configure breakpoints;

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./src/**/*.{vue,js,scss}"
   ],
   theme: {
+    screens: {
+      'sm': '30rem',
+      'md': '48rem',
+      'lg': '64rem',
+      'xl': '80rem'
+    },
+    container: {
+      center: true,
+      padding: '0.8rem'
+    },
     extend: {},
   },
   plugins: [],
