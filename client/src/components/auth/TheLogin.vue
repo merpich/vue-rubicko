b5d8a1fe2af5ff9f6dc0de9cf1adabf0be27a485
merpich <merpich.dev@gmail.com>
2023-03-27 01:08:00 +0300

    add errors;

--- client/src/components/auth/TheLogin.vue
+++ client/src/components/auth/TheLogin.vue
@@ -4,11 +4,13 @@
 			<h2 class="app-title-intro">Вход</h2>
 			<div class="grid">
 				<label class="app-label-intro" for="email">Электронная почта</label>
-				<input class="app-control-intro rounded-2xl" id="email" name="email" type="email">
+				<input class="app-control-intro rounded-2xl" id="email" name="email" type="email" autocomplete="off">
+				<p class="text-red-500">Ошибка</p>
 			</div>
 			<div class="grid">
 				<label class="app-label-intro" for="password">Пароль</label>
 				<input class="app-control-intro rounded-2xl" id="password" name="password" type="password">
+				<p class="text-red-500">Ошибка</p>
 			</div>
 			<button class="app-button-intro rounded-2xl">Войти</button>
 			<RouterLink class="app-link-intro" to="register">Регистрация</RouterLink>
--- client/src/components/auth/TheRegister.vue
+++ client/src/components/auth/TheRegister.vue
@@ -5,14 +5,17 @@
 			<div class="grid">
 				<label class="app-label-intro" for="email">Электронная почта</label>
 				<input class="app-control-intro rounded-2xl" id="email" name="email" type="email">
+				<p class="text-red-500">Ошибка</p>
 			</div>
 			<div class="grid">
 				<label class="app-label-intro" for="password">Пароль</label>
 				<input class="app-control-intro rounded-2xl" id="password" name="password" type="password">
+				<p class="text-red-500">Ошибка</p>
 			</div>
 			<div class="grid">
 				<label class="app-label-intro" for="password_repeat">Повтор пароля</label>
 				<input class="app-control-intro rounded-2xl" id="password_repeat" name="password_repeat" type="password">
+				<p class="text-red-500">Ошибка</p>
 			</div>
 			<button class="app-button-intro rounded-2xl">Зарегистрироваться</button>
 			<RouterLink class="app-link-intro" to="/">Вход</RouterLink>
