fffc08d678aa3a03d1f09e4ec13438fda49dc008
merpich <merpich.dev@gmail.com>
2023-03-27 02:14:07 +0300

    create userStore;

---
+++ client/src/stores/user.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import axios from '@/axios'
+
+export const useUserStore = defineStore('user', () => {
+	const userData = ref({})
+
+	const login = async (data) => {
+		try {
+			const response = await axios.post('/login', data)
+			const { token, ...user } = response.data
+
+			userData.value = { ...user }
+			localStorage.setItem('token', token)
+		} catch (error) {
+			if (error.response) {
+				return error.response.data
+			}
+			return [{ msg: 'Ошибка сети' }]
+		}
+	}
+
+	return { userData, login }
+})
