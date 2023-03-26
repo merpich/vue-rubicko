a6a2402bed2634df98c314af32eb25e719fe288b
merpich <merpich.dev@gmail.com>
2023-03-26 13:32:07 +0300

    update RegisterView;

---
+++ client/src/components/auth/TheRegister.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="app-gutter-intro app-blur rounded-2xl">
+		<form class="app-form-intro" method="post" @submit.prevent>
+			<h2 class="app-title-intro">Регистрация</h2>
+			<div class="grid">
+				<label class="app-label-intro" for="email">Электронная почта</label>
+				<input class="app-control-intro rounded-2xl" id="email" name="email" type="email">
+			</div>
+			<div class="grid">
+				<label class="app-label-intro" for="password">Пароль</label>
+				<input class="app-control-intro rounded-2xl" id="password" name="password" type="password">
+			</div>
+			<div class="grid">
+				<label class="app-label-intro" for="password_repeat">Повтор пароля</label>
+				<input class="app-control-intro rounded-2xl" id="password_repeat" name="password_repeat" type="password">
+			</div>
+			<button class="app-button-intro rounded-2xl">Зарегистрироваться</button>
+			<RouterLink class="app-link-intro" to="/">Вход</RouterLink>
+		</form>
+	</div>
+</template>
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,26 +1,13 @@
+<script setup>
+	import TheRegister from '@/components/auth/TheRegister.vue'
+</script>
+
 <template>
-	<main class="app-page">
-		<div class="grid gap-8 md:gap-16">
-			<div class="logo max-w-[600px] w-full mx-auto">
-				<div class="app-blur app-card app-gutter">
-					<form class="app-form" method="post" @submit.prevent>
-						<h2 class="app-title">Регистрация</h2>
-						<div class="grid">
-							<label class="app-label" for="email">Электронная почта</label>
-							<input class="app-control w-full bg-slate-600/30" id="email" name="email" type="email">
-						</div>
-						<div class="grid">
-							<label class="app-label" for="password">Пароль</label>
-							<input class="app-control w-full bg-slate-600/30" id="password" name="password" type="password">
-						</div>
-						<div class="grid">
-							<label class="app-label" for="password_repeat">Повтор пароля</label>
-							<input class="app-control w-full bg-slate-600/30" id="password_repeat" name="password_repeat" type="password">
-						</div>
-						<button class="app-btn-primary">Зарегистрироваться</button>
-					</form>
-				</div>
-			</div>
+	<main class="app-intro">
+		<div class="app-container">
+			<section class="max-w-[40rem] mx-auto">
+				<TheRegister />
+			</section>
 		</div>
 	</main>
 </template>
