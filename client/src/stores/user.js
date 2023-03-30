d5c23472a6dcb01ab7ba4655534b86313a0eecaf
merpich <merpich.dev@gmail.com>
2023-03-30 09:54:20 +0300

    add get user's data;

--- client/src/stores/user.js
+++ client/src/stores/user.js
@@ -4,6 +4,7 @@ import axios from '@/axios'
 
 export const useUserStore = defineStore('user', () => {
 	const userData = ref({})
+	const isAuth = ref(false)
 
 	const login = async (data) => {
 		try {
@@ -35,5 +36,37 @@ export const useUserStore = defineStore('user', () => {
 		}
 	}
 
-	return { userData, login, register }
+	 const getMe = async (data) => {
+		try {
+			const response = await axios.get('/user', {
+				headers: {
+					Authorization: localStorage.getItem('token')
+				}
+			})
+
+			return response.data
+		} catch (error) {
+
+		}
+	 }
+
+	 const get = async (user) => {
+		try {
+			const response = await axios.get('/user/' + user)
+			const userMe = await getMe()
+
+			userData.value = { ...response.data }
+			isAuth.value = userMe._id === userData.value._id && true
+		} catch (error) {
+
+		}
+	 }
+
+	return {
+		userData,
+		login,
+		register,
+		getMe,
+		get
+	 }
 })
