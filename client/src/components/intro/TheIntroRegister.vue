e8e1f62b28c5cd1c543bfeda30be9e2b59e631e7
merpich <merpich.dev@gmail.com>
2023-03-31 14:30:36 +0300

    update imports;

--- client/src/components/intro/TheIntroRegister.vue
+++ client/src/components/intro/TheIntroRegister.vue
@@ -3,13 +3,10 @@
 	import { useRouter } from 'vue-router'
 	import { useUserStore } from '@/stores/user'
 
-	import IntroCard from '@/components/intro/base/IntroCard.vue'
-	import IntroForm from '@/components/intro/base/IntroForm.vue'
-	import IntroFormTitle from '@/components/intro/base/IntroFormTitle.vue'
-	import IntroFormLabel from '@/components/intro/base/IntroFormLabel.vue'
-	import IntroFormInput from '@/components/intro/base/IntroFormInput.vue'
-	import IntroButton from '@/components/intro/base/IntroButton.vue'
-	import IntroLink from '@/components/intro/base/IntroLink.vue'
+	import {
+		IntroCard, IntroButton, IntroLink, IntroForm,
+		IntroFormTitle, IntroFormLabel, IntroFormInput
+	} from '@/components/intro/base'
 
 	const data = ref({
 		email: '',
