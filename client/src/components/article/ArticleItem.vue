cebdb5de23fbc826db28f416ea6ac586ac31c95b
merpich <merpich.dev@gmail.com>
2023-04-01 15:47:56 +0300

    change border-color;

--- client/src/components/article/ArticleItem.vue
+++ client/src/components/article/ArticleItem.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="px-2 pb-4 grid grid-cols-[1fr_auto] gap-4 border-b-2 border-slate-300">
+	<div class="px-2 pb-4 grid grid-cols-[1fr_auto] gap-4 border-b-2 border-slate-200">
 		<RouterLink class="text-blue-600 font-medium hover:underline" to="#">
 			Название
 		</RouterLink>
