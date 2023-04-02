621f5904a9fadca6c921180b80a89fe6fe5d1990
merpich <merpich.dev@gmail.com>
2023-04-03 01:53:54 +0300

    crate new ui components;

---
+++ client/src/components/base/BaseTextarea.vue
@@ -0,0 +1,3 @@
+<template>
+	<textarea class="py-2 px-4 shadow bg-white rounded-2xl outline-none text-base text-slate-900" name="userBio" id="userBio" cols="30" rows="5"></textarea>
+</template>
--- client/src/components/base/index.js
+++ client/src/components/base/index.js
@@ -2,10 +2,16 @@ import BaseLoader from './BaseLoader.vue'
 import BaseLogo from './BaseLogo.vue'
 import BaseButton from './BaseButton.vue'
 import BaseFile from './BaseFile.vue'
+import BaseLabel from './BaseLabel.vue'
+import BaseInput from './BaseInput.vue'
+import BaseTextarea from './BaseTextarea.vue'
 
 export {
 	BaseLoader,
 	BaseLogo,
 	BaseButton,
-	BaseFile
+	BaseFile,
+	BaseLabel,
+	BaseInput,
+	BaseTextarea
 }
