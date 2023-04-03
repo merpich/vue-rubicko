a653fedb204e0fd8c8e1caa2a0f228b5a54efb2b
merpich <merpich.dev@gmail.com>
2023-04-03 21:09:04 +0300

    add link to settings;

--- client/src/components/profile/TheProfileData.vue
+++ client/src/components/profile/TheProfileData.vue
@@ -1,5 +1,6 @@
 <script setup>
 	import { computed } from 'vue'
+	import { RouterLink } from 'vue-router'
 
 	const props = defineProps({
 		userData: {
@@ -14,9 +15,14 @@
 </script>
 
 <template>
-	<div class="grid sm:gap-1">
-		<h1 class="text-2xl sm:text-3xl text-slate-900 font-bold">{{ userName }}</h1>
-		<p class="text-lg sm:text-xl text-slate-900 font-medium" v-if="fullName">{{ fullName }}</p>
-		<p class="text-sm sm:text-base text-slate-900" v-if="userBio">{{ userBio }}</p>
+	<div class="grid gap-2">
+		<div class="grid sm:gap-1">
+			<h1 class="text-2xl sm:text-3xl text-slate-900 font-bold">{{ userName }}</h1>
+			<p class="text-lg sm:text-xl text-slate-900 font-medium" v-if="fullName">{{ fullName }}</p>
+			<p class="text-sm sm:text-base text-slate-900" v-if="userBio">{{ userBio }}</p>
+		</div>
+		<RouterLink class="text-blue-500 text-sm hover:underline" to="settings">
+			Редактировать
+		</RouterLink>
 	</div>
 </template>
