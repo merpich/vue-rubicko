29416eb03d10f3fad21a830454feb198a008e4b3
merpich <merpich.dev@gmail.com>
2023-04-04 03:00:22 +0300

    create header menu;

--- client/src/components/header/TheHeader.vue
+++ client/src/components/header/TheHeader.vue
@@ -1,11 +1,17 @@
 <script setup>
 	import { BaseLogo } from '../base'
+	import TheHeaderDots from './TheHeaderDots.vue'
+	import TheHeaderMenu from './TheHeaderMenu.vue'
 </script>
 
 <template>
 	<header class="py-2 bg-slate-800 fixed top-0 left-0 right-0 z-10">
-		<div class="container">
+		<div class="container flex justify-between">
 			<BaseLogo />
+			<div class="relative -my-2">
+				<TheHeaderDots />
+				<TheHeaderMenu />
+			</div>
 		</div>
 	</header>
 </template>
---
+++ client/src/components/header/TheHeaderMenu.vue
@@ -0,0 +1,15 @@
+<script setup>
+	import { BaseButton } from '../base'
+</script>
+
+<template>
+	<nav class="absolute top-[calc(100%+1rem)] right-0 p-4 w-72 bg-white shadow rounded-2xl">
+		<ul>
+			<li>
+				<BaseButton class="w-full">
+					Выйти
+				</BaseButton>
+			</li>
+		</ul>
+	</nav>
+</template>
