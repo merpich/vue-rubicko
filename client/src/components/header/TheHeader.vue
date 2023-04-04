2afc89725a1fda93c223d5a445d0af19fa058159
merpich <merpich.dev@gmail.com>
2023-04-05 02:37:59 +0300

    code refactoring;

--- client/src/components/header/TheHeader.vue
+++ client/src/components/header/TheHeader.vue
@@ -1,24 +1,13 @@
 <script setup>
-	import { ref } from 'vue'
 	import { BaseLogo } from '../base'
-	import TheHeaderDots from './TheHeaderDots.vue'
-	import TheHeaderMenu from './TheHeaderMenu.vue'
-
-	const isOpen = ref(false)
-
-	const switchMenu = () => {
-		isOpen.value = !isOpen.value
-	}
+	import TheHeaderControls from './TheHeaderControls.vue'
 </script>
 
 <template>
 	<header class="py-2 bg-slate-800 fixed top-0 left-0 right-0 z-10">
 		<div class="container flex justify-between">
 			<BaseLogo />
-			<div class="relative -my-2">
-				<TheHeaderDots @click="switchMenu" />
-				<TheHeaderMenu v-if="isOpen" />
-			</div>
+			<TheHeaderControls />
 		</div>
 	</header>
 </template>
