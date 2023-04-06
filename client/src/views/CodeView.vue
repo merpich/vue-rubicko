b83c3b6695aef3e7a07d062c1e5293cf4e193f7f
merpich <merpich.dev@gmail.com>
2023-04-06 15:00:34 +0300

    create code preview;

--- client/src/views/CodeView.vue
+++ client/src/views/CodeView.vue
@@ -1,7 +1,11 @@
 <script setup>
 	import TheCodeEditor from '../components/code/TheCodeEditor.vue'
+	import TheCodePreview from '../components/code/TheCodePreview.vue'
 </script>
 
 <template>
-	<TheCodeEditor />
+	<div class="grid grid-rows-[auto_1fr] h-[calc(100%-3.5rem)] overflow-hidden">
+		<TheCodeEditor />
+		<TheCodePreview />
+	</div>
 </template>
